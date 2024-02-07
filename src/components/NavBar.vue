ee14ce5ce32b483cf51a987ba4c7aacb49b59f7f
jonahbearde <bearinparadox@gmail.com>
2024-02-07 18:08:54 +0800

    add a border line in nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="w-[200px] bg-gray-800 min-h-screen flex flex-col py-4 px-2">
-    <RouterLink v-for="route in noAuthRoutes" :key="route.name" :to="route.path"
-      class="flex gap-4 p-2 hover:bg-gray-700 rounded-md cursor-pointer">
-      <img :src="`/icons/${route.meta.iconName}-sharp.svg`" class="w-5 h-auto"/>
-      <p class="font-medium">{{ route.meta?.title }}</p>
-    </RouterLink>
+    <div class="border-b border-slate-600">
+      <RouterLink v-for="route in noAuthRoutes" :key="route.name" :to="route.path"
+        class="flex gap-4 p-2 hover:bg-gray-700 rounded-md cursor-pointer">
+        <img :src="`/icons/${route.meta.iconName}-sharp.svg`" class="w-5 h-auto" />
+        <p class="font-medium">{{ route.meta?.title }}</p>
+      </RouterLink>
+    </div>
+
 
     <RouterLink v-for="route in authRoutes" :key="route.name" :to="route.path"
       class="flex gap-4 p-2 hover:bg-gray-700 rounded-md cursor-pointer">
-      <img :src="`/icons/${route.meta.iconName}-sharp.svg`" class="w-5 h-auto"/>
+      <img :src="`/icons/${route.meta.iconName}-sharp.svg`" class="w-5 h-auto" />
       <p class="font-medium">{{ route.meta?.title }}</p>
     </RouterLink>
   </div>
