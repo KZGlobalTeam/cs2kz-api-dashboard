f9c406f9e724f72d396a3cdf86725ce435b0ba6e
AlphaKeks <alphakeks@dawn.sh>
2024-03-29 14:44:42 +0100

    Only display error debug info if available

--- src/utils.ts
+++ src/utils.ts
@@ -41,9 +41,13 @@ export function renderWorkshopId(workshopId: number) {
 }
 
 export function toErrorMsg(error: any){
-  if(typeof error.response.data === 'object'){
-    return `${error.response.data.debug_info}, ${error.response.data.message}`
-  }else{
+  if (typeof error.response.data !== 'object') {
     return `${error.response.data}`
   }
+
+  if (error.response.data.debug_info) {
+    return `${error.response.data.debug_info}, ${error.response.data.message}`
+  }
+
+  return `${error.response.data.message}`
 }
